61910db51b5b0ac401c8d3eacee13d8e76a0384d
BertCasale <bthebest55@gmail.com>
2023-04-18 18:30:26 -0400

    add routes for all the pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import Navbar from './Components/Navbar.js';
 import Home from './Pages/Home.js';
 import NotFound from './Pages/NotFound.js'
+import BudgetIndex from './Pages/BudgetIndex.js';
+import Show from './Pages/Show.js';
+import New from './Pages/New.js';
+import Edit from './Pages/Edit';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/transactions" element={<BudgetIndex/>}/>
+        <Route path="/transactions/:index" element={<Show/>}/>
+        <Route path="/transactions/new" element={<New/>}/>
+        <Route path="/transactions/:index/edit" element={<Edit/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
