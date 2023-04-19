896b17edc2a6c3ab4cd9a1035e200dce116d9115
BertCasale <bthebest55@gmail.com>
2023-04-18 22:22:46 -0400

    show individual transaction details

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
+
 export default function Show() {
+  const [transaction, setTransaction] = useState({});
+  let navigate = useNavigate();
+  let {index} = useParams();
+
+  useEffect(() => {
+    axios
+    .get(`${API}/transactions/${index}`)
+    .then((res) => {
+      setTransaction(res.data.transactions)
+      console.log(res.data.transactions)
+    })
+    .catch((e) => navigate("/notfound"));
+  },[]);
+
+  //check if the amount is positive or negative, and apply a color 
+  function positiveOrNegative () {
+    return (transaction.amount < 0) ?
+      {color: "red"}:
+      {color: "green"}
+  }
+
   return(<div className="Show">
-    Show
+    <h2>Item details</h2>
+    
+    <article>
+      <h3>{transaction.itemName}</h3>
+      <h4>From: {transaction.from}</h4>
+      <p>Amount: <b style={positiveOrNegative()}>{transaction.amount}</b></p>
+      <p>Category: {transaction.category}</p>
+    </article>
   </div>);
 }
