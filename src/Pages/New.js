e32a1edc19392a74e331c66f8ceaf827b0065edb
BertCasale <bthebest55@gmail.com>
2023-04-18 21:48:03 -0400

    new and edit form layout

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,5 +1,57 @@
 export default function New() {
   return(<div className="New">
-    New
+    <h2>Add a new item</h2>
+
+    <form>
+      <label htmlFor="date">Date:</label>
+      <input 
+        type="date" 
+        id="date" 
+        name="date"
+        required/>
+
+      <label htmlFor="name">Name:</label>
+      <input 
+        type="text"
+        id="name"
+        name="name"
+        placeholder="name" 
+        required/>
+
+      <label htmlFor="amount">Amount:</label>
+      <input 
+        type="number"
+        id="amount"
+        name="amount"
+        placeholder={0} 
+        required/>
+
+      <label htmlFor="from">From:</label>
+      <input 
+        type="text"
+        id="from"
+        name="from"
+        placeholder="from" 
+        required/>
+
+      <label htmlFor="category">Select a Category:</label>
+      <select 
+        name="category" 
+        id="category" 
+        required>
+        <option></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Food">Food</option>
+        <option value="Gas">Gas</option>
+        <option value="Gifts">Gifts</option>
+        <option value="Income">Income</option>
+        <option value="Loan Payment">Loan Payment</option>
+        <option value="Miscellaneous">Miscellaneous</option>
+        <option value="Utilities">Utilities</option>
+      </select>
+
+      <button type="submit">Create New item</button>
+    </form>
   </div>);
 }
