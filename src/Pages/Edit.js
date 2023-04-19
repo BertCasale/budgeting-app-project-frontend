3f4cb399cafb31114821a35a2b926c885a6b9793
BertCasale <bthebest55@gmail.com>
2023-04-19 13:10:50 -0400

    reorganize code

--- src/Pages/Edit.js
+++ src/Pages/Edit.js
@@ -1,57 +1,8 @@
+import TransactionEdit from "../Components/TransactionEdit";
+
 export default function Edit() {
   return(<div className="Edit">
     <h2>Edit item</h2>
-
-    <form>
-      <label htmlFor="date">Date:</label>
-      <input 
-        type="date" 
-        id="date" 
-        name="date"
-        required/>
-
-      <label htmlFor="name">Name:</label>
-      <input 
-        type="text"
-        id="name"
-        name="name"
-        placeholder="name" 
-        required/>
-
-      <label htmlFor="amount">Amount:</label>
-      <input 
-        type="number"
-        id="amount"
-        name="amount"
-        placeholder={0} 
-        required/>
-
-      <label htmlFor="from">From:</label>
-      <input 
-        type="text"
-        id="from"
-        name="from"
-        placeholder="from" 
-        required/>
-
-      <label htmlFor="category">Select a Category:</label>
-      <select 
-        name="category" 
-        id="category" 
-        required>
-        <option></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Entertainment">Entertainment</option>
-        <option value="Food">Food</option>
-        <option value="Gas">Gas</option>
-        <option value="Gifts">Gifts</option>
-        <option value="Income">Income</option>
-        <option value="Loan Payment">Loan Payment</option>
-        <option value="Miscellaneous">Miscellaneous</option>
-        <option value="Utilities">Utilities</option>
-      </select>
-
-      <button type="submit">Create New item</button>
-    </form>
+    <TransactionEdit/>
   </div>);
 }
--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,54 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-const API = process.env.REACT_APP_API_URL;
+import TransactionDetails from "../Components/TransactionDetails";
 
 export default function Show() {
-  const [transaction, setTransaction] = useState({});
-  let navigate = useNavigate();
-  let {index} = useParams();
-
-  useEffect(() => {
-    axios
-    .get(`${API}/transactions/${index}`)
-    .then((res) => {
-      setTransaction(res.data.transactions)
-      console.log(res.data.transactions)
-    })
-    .catch((e) => navigate("/notfound"));
-  },[]);
-
-  //check if the amount is positive or negative, and apply a color 
-  function positiveOrNegative () {
-    return (transaction.amount < 0) ?
-      {color: "red"}:
-      {color: "green"}
-  }
-
-  function deleteTransaction() {
-    axios
-    .delete(`${API}/transactions/${index}`)
-    .then(() => {
-      navigate("/transactions");
-    })
-    .catch((e) => console.error("catch", e));
-  }
 
   return(<div className="Show">
     <h2>Item details</h2>
-    
-    <article>
-      <h3>{transaction.itemName}</h3>
-      <h4>From: {transaction.from}</h4>
-      <p>Amount: <b style={positiveOrNegative()}>{transaction.amount}</b></p>
-      <p>Category: {transaction.category}</p>
-    </article>
-
-    <Link to={`/transactions/${index}/edit`}>
-      <button>Edit</button>
-    </Link>
-
-    <button onClick={deleteTransaction}>Delete</button>
+    <TransactionDetails/>
   </div>);
 }
